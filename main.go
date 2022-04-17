384986ddec595f5fb8136d293fc6d764c11dfa77
taiti09 <taitiabe1997@gmail.com>
2022-04-17 22:15:08 +0900

    first commit

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import "fmt"
 
 const ROW_NUM = 3
 const COLUMN_NUM = 3
+const PLAYER1 = 1
+const PALYER2 = 2
 
 type pos struct {
 	x int
@@ -18,14 +20,17 @@ func main() {
 	for {
 		InputPlayer1(&board)
 		PrintBoard(&board)
+		if (Is_win(&board,PLAYER1)) {
+			fmt.Printf("Player 1 is Win\n")
+			break
+		}
 		InputPlayer2(&board)
 		PrintBoard(&board)
-		if (Is_win(&board)) {
+		if (Is_win(&board,PALYER2)) {
+			fmt.Printf("Player 2 is Win\n")
 			break
 		}
 	}
-	PrintBoard(&board)
-
 }
 
 func Init_Board(board *[][]int){
@@ -49,35 +54,35 @@ func PrintBoard(board *[][]int){
 	}
 }
 
-func Row_check(board *[][]int) bool{
+func Row_check(board *[][]int,player int) bool{
 	for i := 0;i < ROW_NUM;i++ {
-		if ((*board)[i][0] == (*board)[i][1] && (*board)[i][1] == (*board)[i][2] && (*board)[i][0] == (*board)[i][2]){
+		if (((*board)[i][0] == (*board)[i][1] && (*board)[i][1] == (*board)[i][2] && (*board)[i][0] == (*board)[i][2]) && (*board)[i][0] == player){
 			return true
 		}
 	}
 	return false
 }
 
-func Column_check(board *[][]int) bool{
+func Column_check(board *[][]int,player int) bool{
 	for i := 0;i < COLUMN_NUM;i++ {
-		if ((*board)[0][i] == (*board)[1][i] && (*board)[1][i] == (*board)[2][i] && (*board)[0][i] == (*board)[2][i]){
+		if (((*board)[0][i] == (*board)[1][i] && (*board)[1][i] == (*board)[2][i] && (*board)[0][i] == (*board)[2][i]) && (*board)[0][i] == player){
 			return true
 		}
 	}
 	return false
 }
 
-func Cross_check(board *[][]int) bool{
-	if ((*board)[0][0] == (*board)[1][1] && (*board)[1][1] == (*board)[2][2] && (*board)[0][0] == (*board)[2][2]){
+func Cross_check(board *[][]int,player int) bool{
+	if (((*board)[0][0] == (*board)[1][1] && (*board)[1][1] == (*board)[2][2] && (*board)[0][0] == (*board)[2][2]) && (*board)[0][0] == player){
 		return true
-	}else if ((*board)[0][2] == (*board)[1][1] && (*board)[1][1] == (*board)[2][0] && (*board)[0][2] == (*board)[2][0]){
+	}else if (((*board)[0][2] == (*board)[1][1] && (*board)[1][1] == (*board)[2][0] && (*board)[0][2] == (*board)[2][0]) && (*board)[0][2] == player){
 		return true
 	}
 	return false
 }
 
-func Is_win(board *[][]int) bool{
-	if (Row_check(board) == true || Column_check(board) == true || Cross_check(board) == true) {
+func Is_win(board *[][]int,player int) bool{
+	if (Row_check(board,player) == true || Column_check(board,player) == true || Cross_check(board,player) == true) {
 		return true
 	}
 	return false
@@ -86,7 +91,9 @@ func Is_win(board *[][]int) bool{
 func InputPlayer1(board *[][]int) {
 	var P1 pos
 
-	fmt.Scanf("Player 1 (x y): %d %d",&P1.x,&P1.y)
+	fmt.Printf("Player 1 (x y) : ")
+	fmt.Scanf("%d %d",&P1.x,&P1.y)
+	fmt.Printf("x:%d,y:%d\n",P1.x,P1.y)
 	if (Is_empty(board,P1.x,P1.y)) {
 		(*board)[P1.x][P1.y] = 1
 	}
@@ -95,7 +102,8 @@ func InputPlayer1(board *[][]int) {
 func InputPlayer2(board *[][]int) {
 	var P2 pos
 
-	fmt.Scanf("Player 2 (x y) : %d %d",&P2.x,&P2.y)
+	fmt.Printf("Player 2 (x y) : ")
+	fmt.Scanf("%d %d",&P2.x,&P2.y)
 	if (Is_empty(board,P2.x,P2.y)) {
 		(*board)[P2.x][P2.y] = 2
 	}
