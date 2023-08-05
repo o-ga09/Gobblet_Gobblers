b5cfd0f26f1f99470316f61b86efb9370ac022f6
taiti09 <taitiabe1997@gmail.com>
2023-08-05 09:58:49 +0900

    #4 駒のサイズの定数追加

---
+++ frontend/src/lib/util/const.ts
@@ -0,0 +1,6 @@
+export const KOMA_LARGE_1 = 5;
+export const KOMA_LARGE_2 = 4;
+export const KOMA_MEDIUM_1 = 3;
+export const KOMA_MEDIUM_2 = 2;
+export const KOMA_SMALL_1 = 1;
+export const KOMA_SMALL_2 = 0;
